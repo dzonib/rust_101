70faa246e51f259d8a48a2eb214a77a2703f439c
dzonib <nikolabosnjak381@gmail.com>
2019-03-04 22:40:40 +0100

    arrays done vectors next

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 // mod print;
 // mod vars;
 // mod types;
-mod strings;
+// mod strings;
+// mod tuples;
+mod arrays;
 
 
 fn main() {
     // print::run();
     // vars::run();
     // types::run();
-    strings::run();
+    // strings::run();
+    arrays::run();
 }
